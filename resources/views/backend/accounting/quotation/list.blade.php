6c51f1b62a1dbc79aaebd6d23616c01d02dd39df
Robert Aponte <cws2097@gmail.com>
2026-04-28 12:39:08 -0400

    PP-39 modificaciones

--- resources/views/backend/accounting/quotation/list.blade.php
+++ resources/views/backend/accounting/quotation/list.blade.php
@@ -40,9 +40,59 @@
 @section('js-script')
 <script>
 	$(function() {
-        $('#quotation-table').DataTable({
-            processing: true,
+		
+		
+		$('#quotation-table thead tr').clone(true).appendTo('#quotation-table thead');
+            $('#quotation-table thead tr:eq(1) th').each(function(i) {
+                var title = $(this).text(); //es el nombre de la columna
+                if (i != 0 && i != 3 && i != 7) {
+					
+					if (i == 6) {
+							$(this).html('<select style="width:100%;" class="form-control filtros"><option value="">Todas</option> <option value="pendiente">Pendiente</option> <option value="anulada">Anulada</option><option value="convertida">Convertida</option></select>');
+							$('.filtros', this).on('change', function () {
+								  if (quotation_table.column(i).search() !== this.value) {
+				
+									quotation_table
+										.column(i)
+										.search(this.value)
+										.draw();
+								}
+				
+							});
+					}else if(i == 1){
+						$(this).html( '<input style="width:100%;" type="text" id="fecha_ingreso" name="fecha_ingreso" value="" class="form-control select-filter" placeholder="Search...'+title+'" />' );
+					}else{
+							$(this).html(
+								'<input style="width:100%;" type="text" value="" class="form-control filtros" placeholder="Search...' +
+								title + '" />');
+
+							$('.filtros', this).on('change', function() {
+								if (quotation_table.column(i).search() !== this.value) {
+
+									quotation_table
+										.column(i)
+										.search(this.value)
+										.draw();
+								}
+
+							});
+					}
+					
+                } else {
+						$(this).html('');
+                }
+
+            });
+		
+        var quotation_table = $('#quotation-table').DataTable({
+			processing: true,
             serverSide: true,
+            orderCellsTop: true,
+            fixedHeader: true,
+			bStateSave: true,
+            bAutoWidth: false,
+            ordering: false,
+            searching: true,
 			ajax: '{{ url('reservas/get_table_data') }}',
 			"columns" : [
 				{ data : "quotation_number", name : "quotation_number" },
@@ -54,6 +104,17 @@
 				{ data : "status", name : "status" },
 				{ data : "action", name : "action" },
 			],
+			dom: 'Bfrltip',
+                buttons: [
+				{
+                    text: 'Reset Filter',
+                    action: function(e, dt, node, config) {
+                        $('.filtros').val('');
+						$('.select-filter').val(null).trigger('change');
+							quotation_table.search('').columns().search('').draw();
+                      }
+                }],
+			lengthMenu: [[ 20, 50, 500, 1000 ], [20,50, 500, 1000 ]],	
 			responsive: true,
 			"bStateSave": true,
 			"bAutoWidth":false,	
@@ -79,6 +140,50 @@
 				}
 			} 
         });
+		
+		 $('#fecha_ingreso').daterangepicker({
+            autoUpdateInput: false,
+            locale: {
+                format: 'YYYY-MM-DD',
+                cancelLabel: 'Clear'
+            }
+        });
+
+         $('#fecha_ingreso').on('change', function(e) {
+            let val = $(this).val();
+            quotation_table.columns(1).search(val ? val : '', true, false );
+            quotation_table.draw();
+        });
+    
+    
+        $('#fecha_ingreso').on('apply.daterangepicker', function(ev, picker) {
+                let daterango =(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+                    $(this).val(daterango);
+                    quotation_table.columns(1).search(daterango);
+                    quotation_table.draw();
+            });
+
+            $('#fecha_ingreso').on('cancel.daterangepicker', function(ev, picker) {
+                $('#fecha_ingreso').val(null).trigger('change');    
+        });
+		
+		    $('.dataTables_filter input')
+    .unbind('keypress keyup input')
+    .bind('change input', function (e) {
+        if ($(this).val().length >= 3 && e.keyCode == 13) {
+            table.search(this.value).draw();
+        }
+    });
+  
+  $('#quotation-table').on('processing.dt', function (e, settings, processing) {
+    if (processing) {
+       	inicioLoading();
+    } else {
+        closeLoading();
+    }
+});
+		
+		
     });
 </script>
 @endsection
